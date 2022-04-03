fe7b14e366048635d1ee406a758e86d3c22415b1
katroll <kroll256@GMAIL.COM>
2022-04-03 18:47:27 -0400

    CORS origins add heroku domain

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:4000"
+    origins "http://localhost:4000, https://morning-scrubland-82075.herokuapp.com"
 
     resource "*",
       headers: :any,
