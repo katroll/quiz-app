3cf7d6df63343cc631a46d114d0f8683e64f4ab0
katroll <kroll256@GMAIL.COM>
2022-04-03 20:14:37 -0400

    session domain

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionController::CgiRequest::DEFAULT_SESSION_OPTIONS[:session_domain => '.herokuapp.com']
