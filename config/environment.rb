816b4def7ee2e17148694e1da34033d964737d5e
katroll <kroll256@GMAIL.COM>
2022-04-03 20:17:36 -0400

    session domain removed

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,6 @@
 # Load the Rails application.
 require_relative "application"
 
-ActionController::CgiRequest::DEFAULT_SESSION_OPTIONS[:session_domain => 'https://morning-scrubland-82075.herokuapp.com']
-
 
 # Initialize the Rails application.
 Rails.application.initialize!
