c8b75c9852aa56a7526178c00dcc394d476b2774
katroll <kroll256@GMAIL.COM>
2022-04-04 20:23:20 -0400

    cookie

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
         user = User.find_by(username: params[:username])
         
         if user&.authenticate(params[:password])
-            session[:user_id] = user.id
+            # session[:user_id] = user.id
             cookies[:user_id] = user.id
             render json: user, status: :created
         else
