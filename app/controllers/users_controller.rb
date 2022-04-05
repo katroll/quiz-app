87397c8b51471f54d6371e3c70165db803c1c3f8
katroll <kroll256@GMAIL.COM>
2022-04-04 20:07:20 -0400

    added domain to session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
 
   # GET /users/1
   def show
-    user = User.find_by(id: session[:user_id])
+    user = User.find_by(id: session[:user_id][:value])
     if user
       render json: user
     else
