1c51008532817cd75731196e1f54480d1b07d094
katroll <kroll256@GMAIL.COM>
2022-04-04 20:10:33 -0400

    added domain to session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
         user = User.find_by(username: params[:username])
         
         if user&.authenticate(params[:password])
-            session[:user_id] = {value: user.id, domain: "https://morning-scrubland-82075.herokuapp.com"}
+            session[:user_id] = {value: user.id, domain: "spctc.org"}
             render json: user, status: :created
         else
             render json: { error: "Invalid username or password" }, status: :unauthorized
