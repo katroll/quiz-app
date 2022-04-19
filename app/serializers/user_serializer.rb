a50eacee84ccf571981220e5fb344d674dc4d29c
katroll <kroll256@GMAIL.COM>
2022-04-19 13:19:49 -0400

    changed user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,6 +2,5 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :username, :admin, :role, :created_at
 
   has_many :grades
-  has_many :users_classes
-  has_many :spctc_classes, through: :users_classes
+  has_many :spctc_classes
 end
