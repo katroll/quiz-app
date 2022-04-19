3ba37bba217efea6ce097272e9ed370943abb675
katroll <kroll256@GMAIL.COM>
2022-04-19 14:02:21 -0400

    added relations to spctc_class model and serializer

--- app/models/spctc_class.rb
+++ app/models/spctc_class.rb
@@ -1,2 +1,7 @@
 class SpctcClass < ApplicationRecord
+    has_many :users_classes
+    has_many :users, through: :users_classes
+
+    has_many :quizzes_classes
+    has_many :quizzes, through: :quizzes_classes
 end
--- app/serializers/spctc_class_serializer.rb
+++ app/serializers/spctc_class_serializer.rb
@@ -1,3 +1,6 @@
 class SpctcClassSerializer < ActiveModel::Serializer
   attributes :id, :name
+
+  has_many :users
+  has_many :quizzes
 end
