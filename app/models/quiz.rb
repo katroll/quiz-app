dac5e1f7b38aa14ede4be946afc68760f8ec39b0
katroll <kroll256@GMAIL.COM>
2022-04-24 14:27:33 -0400

    added dependent destroy to quizzes_classes in quiz model

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,6 +2,6 @@ class Quiz < ApplicationRecord
     has_many :questions, dependent: :destroy
     has_many :grades, dependent: :destroy
 
-    has_many :quizzes_classes
+    has_many :quizzes_classes, dependent: :destroy
     has_many :spctc_classes, through: :quizzes_classes
 end
