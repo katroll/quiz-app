904c48efb5734829de55247397aea30b4252ab9f
katroll <kroll256@GMAIL.COM>
2022-04-26 12:52:09 -0400

    added routes/actions to remove students/quizzes from class

--- app/models/quizzes_class.rb
+++ app/models/quizzes_class.rb
@@ -3,6 +3,6 @@ class QuizzesClass < ApplicationRecord
   belongs_to :spctc_class
 
   def self.find_quizzes_class(spctc_class_id, quiz_id)
-    QuizzesClass.where(quiz_id: quiz_id).where(spctc_class_id: spctc_class_id).id
+    QuizzesClass.where(quiz_id: quiz_id).where(spctc_class_id: spctc_class_id).first.id
   end
 end
--- app/models/users_class.rb
+++ app/models/users_class.rb
@@ -4,7 +4,7 @@ class UsersClass < ApplicationRecord
 
   def self.find_users_class(spctc_class_id, user_id)
     puts "in the model"
-    value = UsersClass.where(user_id: user_id).where(spctc_class_id: spctc_class_id).id
+    value = UsersClass.where(user_id: user_id).where(spctc_class_id: spctc_class_id).first.id
     puts "value: #{value}"
     
     value
